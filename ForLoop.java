127b1c3fbaabd79796418bb20e7d268224cd8ac5
Sujan Adhikari <151158221+knox-sd@users.noreply.github.com>
2024-01-29 23:16:28 +0300

    Add files via upload
    add for loop code in Java

---
+++ ForLoop.java
@@ -0,0 +1,10 @@
+public class ForLoop {
+    public static void main(String[] args){
+        // for loop = Executes a block of code a limited amount of times
+        // for (int i = 0; i <=10; i++){
+        for (int i = 10; i >=0; i--){
+            System.out.println(i);
+        }
+        System.out.println("Happy New Year!");
+    }
+}
